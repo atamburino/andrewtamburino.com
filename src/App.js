613836ba2f8d72b5f125d29bf0e7c8728968f7d6
atamburino <andytamburino@gmail.com>
2024-11-02 18:16:55 -0400

    nav bar created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Navbar } from "./components/common";
 import "./App.css";
 
+
 class App extends Component {
   render() {
     return(
--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import './Navbar.css';
 
 class Navbar extends Component {
   render() {
     return(
-        <p>
-            This is a Navbar
-        </p>
+      <nav className="main-nav">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/">Resume</a></li>
+              <li><a href="/">Contact</a></li>
+            </ul>
+          </nav>
     )
   }
 }
