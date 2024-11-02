48db1a280852b017938b8b4ddf791a79a95c606b
atamburino <andytamburino@gmail.com>
2024-11-02 17:36:34 -0400

    created common folder and nav files

---
+++ src/components/common/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return(
+        <p>
+            This is a Navbar
+        </p>
+    )
+  }
+}
+
+export default Navbar;
