51bd21a9f263a1c290ff5bf68481e57cfe61733d
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2025-01-14 22:19:39 -0500

    stripped down old prodject

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React, { Component } from "react";
-import HomePage from "./pages/homepage/HomePage";
-import { 
-  Contact,
-  Footer,
-  Intro,
-  Navbar,
-  Portfolio,
-  Timeline,
-  Title
-} from "./components/common";
+import React from 'react';
+import './App.css';
 
-import "./App.css";
-
-
-class App extends Component {
-  render() {
-    return(
-      <div>
-        <Navbar />
-        <Intro />
-				<Portfolio />
-				<Timeline />
-				<Contact />
-        <HomePage />
-        <Footer />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is a test</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
